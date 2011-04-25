cd8804adcf346673cdcb03f04df5095c8052f09e
Murilo Cadanus da Costa <murilocadanus@gmail.com>
2011-04-25 09:29:46 -0300

    Initial Commit

---
+++ ViaLacteum/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  ViaLacteum
+//
+//  Created by Murilo Costa on 25/04/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main (int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
