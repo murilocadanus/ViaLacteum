4a1c8dc769531147b2714b662a6034110b0f781d
Murilo Cadanus da Costa <murilocadanus@gmail.com>
2011-05-17 14:14:04 -0300

    Added waves

--- ViaLacteum/foe.m
+++ ViaLacteum/foe.m
@@ -144,7 +144,8 @@ void move_foes()
                         all_foes[i].clear = clear_explosion_1;
                         all_foes[i].type = AT_LAST;
                     break;
-                }
+                }                
+                
             break;
                 
             case AT_LAST:
